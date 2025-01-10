a808a4c2b3cae38bac8e13aaa8ae85032cd9b744
warghostaka <2915701431@qq.com>
2025-01-10 14:46:10 +0800

    new

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+	<!-- 修改语言为中文 -->
+	<el-config-provider :locale="zhCN">
+		<router-view></router-view>
+	</el-config-provider>
+</template>
+
+<script setup>
+import { zhCn } from 'element-plus/lib/locale/lang/zh-cn';
+</script>
